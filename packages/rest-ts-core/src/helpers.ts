d16ea84bc92d26cd88c74610d2e3cd5d5fd06a9a
Hadrien Milano <hadrien.milano@gmail.com>
2018-10-28 22:38:55 +0100

    refactor helper functions

--- packages/rest-ts-core/src/helpers.ts
+++ packages/rest-ts-core/src/helpers.ts
@@ -1,16 +1,23 @@
 import { EndpointDefinition } from './base';
 
 export function getPathWithParams(def: EndpointDefinition): string {
-    let i = 1;
-    return def.path[0] + Object.keys(def.params || {}).map((k) => `:${k}` + def.path[i++]).join('');
+    let pathBuilder = def.path[0];
+    if (isNotUndefined(def.params)) {
+        for (let i = 0 ; i < def.params.length ; i++) {
+            pathBuilder += ':' + def.params[i] + def.path[i+1];
+        }
+    }
+    return pathBuilder;
 }
 
 export function makePathWithParams<T extends EndpointDefinition>(def: T, params: {[key: string]: string} | undefined): string {
-    if (isNotUndefined(def.params) && isNotUndefined(params) ) {
-        let i = 1;
-        return def.path[0] + def.params.map((p) => params[p] + def.path[i++]).join('');
+    let pathBuilder = def.path[0];
+    if (isNotUndefined(def.params) && isNotUndefined(params)) {
+        for (let i = 0 ; i < def.params.length ; i++) {
+            pathBuilder += params[def.params[i]] + def.path[i+1];
+        }
     }
-    return def.path[0];
+    return pathBuilder;
 }
 
 type NotUndefined<T> = T extends undefined ? never : T;
