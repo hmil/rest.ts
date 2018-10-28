a8548e0bac1827c17b1d5a9928ef2c7f82f7e0a3
Hadrien Milano <hadrien.milano@gmail.com>
2018-10-28 18:31:21 +0100

    Fix double slash issue

--- packages/rest-ts-core/src/helpers.ts
+++ packages/rest-ts-core/src/helpers.ts
@@ -2,22 +2,15 @@ import { EndpointDefinition } from './base';
 
 export function getPathWithParams(def: EndpointDefinition): string {
     let i = 1;
-    return def.path[0] + trailingSlashIfNeeded(def.params) + Object.keys(def.params || {}).map((k) => `:${k}` + def.path[i++]).join('');
+    return def.path[0] + Object.keys(def.params || {}).map((k) => `:${k}` + def.path[i++]).join('');
 }
 
 export function makePathWithParams<T extends EndpointDefinition>(def: T, params: {[key: string]: string} | undefined): string {
     if (isNotUndefined(def.params) && isNotUndefined(params) ) {
         let i = 1;
-        return def.path[0] + trailingSlashIfNeeded(def.params) + def.params.map((p) => params[p] + def.path[i++]).join('');
+        return def.path[0] + def.params.map((p) => params[p] + def.path[i++]).join('');
     }
     return def.path[0];
-}
-
-function trailingSlashIfNeeded(params?: string[]) {
-    if (params != null && params.length > 0) {
-        return '/';
-    }
-    return '';
 }
 
 type NotUndefined<T> = T extends undefined ? never : T;
