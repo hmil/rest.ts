65d94561ff039c32e1bde1d6a5930bb3d4e581ff
Hadrien Milano <hadrien.milano@gmail.com>
2018-12-02 00:33:46 +0100

    Add release boilerplate

--- Makefile
+++ Makefile
@@ -2,15 +2,25 @@
 NODE_MODULES=node_modules/.makets
 
 PACKAGES=$(wildcard packages/*)
-TASKS=build lint
+TASKS=build lint publish
+
+publish: pre-publish-check
 
 packages/rest-ts-express: packages/rest-ts-core
 packages/rest-ts-axios: packages/rest-ts-core
 test/e2e-runtypes: packages/rest-ts-express packages/rest-ts-axios
 test/e2e-vanilla: packages/rest-ts-express packages/rest-ts-axios
 
+pre-publish-check:
+	if [ -z "$$TRAVIS" ]; then echo "The publish task may only run in travis. Did you mean 'release'?" && exit 1; fi
+
+.PHONY: release
+release: test
+	npm run release
+
 .PHONY: test
 test: $(NODE_MODULES)
+	$(MAKE) build
 	npm test
 
 $(NODE_MODULES): package.json package-lock.json
---
+++ release.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+import * as inquirer from 'inquirer';
+import * as path from 'path';
+import { cd, echo, exec } from 'shelljs';
+
+const prompt = inquirer.createPromptModule();
+
+function updatePackageJSON(pkgJSONLocation: string, version: string) {
+    const pkgJSON = require(pkgJSONLocation);
+    pkgJSON.version = version;
+    if (pkgJSON.peerDependencies) {
+        for (let i of Object.keys(pkgJSON.peerDependencies)) {
+            if (i.startsWith('rest-ts')) {
+                pkgJSON.peerDependencies[i] = `^${version}`;
+            }
+        }
+    }
+    fs.writeFileSync(pkgJSONLocation, JSON.stringify(pkgJSON, null, 4), { encoding: 'utf-8' });
+}
+
+(async () => {
+
+    if (exec('git status --porcelain').stdout.trim() !== '') {
+        echo('Your repository is dirty. Aborting');
+        process.exit(1);
+        return;
+    }
+
+    const { version, message } = await prompt<{version: string, message: string}>([{
+        type: 'input',
+        name: 'version',
+        message: 'Version?',
+        filter: (val) => {
+        return val.toLowerCase();
+        }
+    }, {
+        type: 'input',
+        name: 'message',
+        message: 'Short release message'
+    }]);
+
+    echo(`About to release ${version}: ${message}.`);
+
+    const { proceed } = await prompt<{proceed: boolean}>([{
+        type: 'confirm',
+        name: 'proceed',
+        message: 'Proceed?',
+        default: false
+    }]);
+
+    if (proceed !== true) {
+        echo('Aborting');
+        process.exit(1);
+        return;
+    }
+
+    updatePackageJSON('./packages/rest-ts-core/package.json', version);
+    updatePackageJSON('./packages/rest-ts-axios/package.json', version);
+    updatePackageJSON('./packages/rest-ts-express/package.json', version);
+
+    // exec(`git add -u && git commit -m "release v${version}"`);
+    // exec(`git tag -s -m "${message}" "v${version}"`);
+    // exec('git push --tags origin master');
+})();
